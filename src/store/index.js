802d5fbcbae972338b88ab1aac81997f53515e1a
Kat <avchimchik@gmail.com>
2025-06-14 17:57:27 +0500

    Store создан

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import reducer from '../reducers';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = configureStore({
+    reducer: reducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+    devTools: process.env.NODE_ENV !== 'production'
+}) 
 
 export default store;
